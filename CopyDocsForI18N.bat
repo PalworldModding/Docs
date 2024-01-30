46723304960416582e00dea6e296b111bd5ed7e0
ItzDabbzz <ItzDabbzz@Gmail.com>
2024-01-30 05:08:06 -0600

    Update Copy Bat

--- CopyDocsForI18N.bat
+++ CopyDocsForI18N.bat
@@ -1,12 +1,25 @@
-xcopy src\pages\**.md i18n\fr\docusaurus-plugin-content-pages /s /e /y /v
-xcopy src\pages\**.mdx i18n\fr\docusaurus-plugin-content-pages /s /e /y /v
-xcopy blog\** i18n\fr\docusaurus-plugin-content-blog /s /e /y /v
-xcopy src\pages\**.md i18n\ja\docusaurus-plugin-content-pages /s /e /y /v
-xcopy src\pages\**.mdx i18n\ja\docusaurus-plugin-content-pages /s /e /y /v
-xcopy blog\** i18n\ja\docusaurus-plugin-content-blog /s /e /y /v
-xcopy src\pages\**.md i18n\ru\docusaurus-plugin-content-pages /s /e /y /v
-xcopy src\pages\**.mdx i18n\ru\docusaurus-plugin-content-pages /s /e /y /v
-xcopy blog\** i18n\ru\docusaurus-plugin-content-blog /s /e /y /v
-xcopy src\pages\**.md i18n\uk\docusaurus-plugin-content-pages /s /e /y /v
-xcopy src\pages\**.mdx i18n\uk\docusaurus-plugin-content-pages /s /e /y /v
-xcopy blog\** i18n\uk\docusaurus-plugin-content-blog /s /e /y /v
+xcopy src\pages\**.md i18n\de\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.mdx i18n\de\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.md i18n\de\docusaurus-plugin-content-docs\current /s /e /y
+xcopy src\pages\**.mdx i18n\de\docusaurus-plugin-content-docs\current /s /e /y
+xcopy blog\** i18n\de\docusaurus-plugin-content-blog /s /e /y
+xcopy src\pages\**.md i18n\fr\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.mdx i18n\fr\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.md i18n\fr\docusaurus-plugin-content-docs\current /s /e /y
+xcopy src\pages\**.mdx i18n\fr\docusaurus-plugin-content-docs\current /s /e /y
+xcopy blog\** i18n\fr\docusaurus-plugin-content-blog /s /e /y
+xcopy src\pages\**.md i18n\ja\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.mdx i18n\ja\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.md i18n\ja\docusaurus-plugin-content-docs\current /s /e /y
+xcopy src\pages\**.mdx i18n\ja\docusaurus-plugin-content-docs\current /s /e /y
+xcopy blog\** i18n\ja\docusaurus-plugin-content-blog /s /e /y
+xcopy src\pages\**.md i18n\ru\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.mdx i18n\ru\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.md i18n\ru\docusaurus-plugin-content-docs\current /s /e /y
+xcopy src\pages\**.mdx i18n\ru\docusaurus-plugin-content-docs\current /s /e /y
+xcopy blog\** i18n\ru\docusaurus-plugin-content-blog /s /e /y
+xcopy src\pages\**.md i18n\uk\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.mdx i18n\uk\docusaurus-plugin-content-pages /s /e /y
+xcopy src\pages\**.md i18n\uk\docusaurus-plugin-content-docs\current /s /e /y
+xcopy src\pages\**.mdx i18n\uk\docusaurus-plugin-content-docs\current /s /e /y
+xcopy blog\** i18n\uk\docusaurus-plugin-content-blog /s /e /y
