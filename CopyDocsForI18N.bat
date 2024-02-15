ef61529ae9f515999fe76619ceefdfcdff7e7896
MoRanYue <2627706725@qq.com>
2024-02-15 22:27:21 +0800

    add json file type to copying script

--- CopyDocsForI18N.bat
+++ CopyDocsForI18N.bat
@@ -1,13 +1,13 @@
 set params=/s /e /y
 
 for %%l in (de,fr,ja,ru,uk,zh-hans) do (
-  for %%f in (md,mdx) do (
+  for %%f in (md,mdx,json) do (
     xcopy src\pages\**.%%f i18n\%%l\docusaurus-plugin-content-pages %params%
   )
-  for %%f in (jpg,png,gif) do (
+  for %%f in (jpg,png,gif,json) do (
     xcopy src\pages\**.%%f i18n\%%l\docusaurus-plugin-content-docs\current %params%
   )
-  for %%f in (md,mdx,jpg,png,gif,css) do (
+  for %%f in (md,mdx,jpg,png,gif,css,json) do (
     xcopy docs\**.%%f i18n\%%l\docusaurus-plugin-content-docs\current %params%
   )
   xcopy blog\** i18n\%%l\docusaurus-plugin-content-blog %params%
