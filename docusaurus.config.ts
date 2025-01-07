2e7dcd536c12b953a9c7a4620f59891aa57028a0
Dabz <itzdabbzz@gmail.com>
2025-01-07 04:40:56 -0600

    Removed blog from navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -118,7 +118,7 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/PalworldModding',
           label: 'GitHub',
